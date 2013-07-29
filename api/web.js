eda8f824c063480adeaf4522ce01935de00a2150
Sai Shenoy <sshenoy@kabam.com>
2013-07-29 00:43:28 -0700

    play test api on port 5000

---
+++ api/web.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var iniparser = require('iniparser');
+var sanitizer = require("sanitizer");
+var mysql      = require('mysql');
+var config = iniparser.parseSync('../configs/db.ini');
+var app = express();
+app.use(express.logger());
+
+
+
+var connection = mysql.createConnection({
+  host     : config.mysql_host,
+  user     : config.mysql_user,
+  password : config.mysql_pass,
+  database: config.mysql_db
+});
+
+connection.connect();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+
+app.get('/bestrate/:amount_in_usd',function(req,res){
+	var amount_in_usd = sanitizer.escape(req.params.amount_in_usd);
+	var sql = "select provider,round(rate*"+amount_in_usd+"-india_fee*"+amount_in_usd+",2) as remittance_amount,round("+amount_in_usd+"+flat_fee,2) as cost, round((rate*"+amount_in_usd+"-india_fee*"+amount_in_usd+")/("+amount_in_usd+"+flat_fee),2) as effective_rate from exchange_rate_daily where date = CURDATE() and "+amount_in_usd+" between slab_start and slab_end order by effective_rate desc";
+	var query = connection.query(sql, function(err, rows, fields) {
+		res.send(JSON.stringify(rows));  
+	});
+	console.log(query.sql);
+	
+});
+
+var port = 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});

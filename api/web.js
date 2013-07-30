e400cf9bbca19410ce73d0c28fb95cb59582be59
Sai Shenoy <sshenoy@kabam.com>
2013-07-30 01:00:36 -0700

    Added server side google analytics

--- api/web.js
+++ api/web.js
@@ -29,6 +29,14 @@ app.configure(function(){
 app.get('/bankrate',function(req,res){
 	var sql = "select * from bank_rate where date = CURDATE()";
 	var query = connection.query(sql, function(err, rows, fields) {
+		var GA = require("googalytics");
+		GA.initialize('UA-42619372-2', 'trackathon.com', function () {
+		  GA.trackPage('Inter Bank Rates', '/bankrate', function (err, resp) {
+		    if (!err, resp.statusCode === 200) {
+		      console.log('it worked!');
+		    }
+		  });
+		});
 		res.send(JSON.stringify(rows));  
 	});
 	console.log(query.sql);
@@ -40,6 +48,14 @@ app.get('/bestrate/:amount_in_usd',function(req,res){
 	var amount_in_usd = sanitizer.escape(req.params.amount_in_usd);
 	var sql = "select provider,round(rate*"+amount_in_usd+"-india_fee*"+amount_in_usd+",2) as remittance_amount_in_inr,round("+amount_in_usd+"+flat_fee,2) as cost_in_usd, round((rate*"+amount_in_usd+"-india_fee*"+amount_in_usd+")/("+amount_in_usd+"+flat_fee),2) as effective_rate from exchange_rate_daily where date = CURDATE() and "+amount_in_usd+" between slab_start and slab_end order by effective_rate desc";
 	var query = connection.query(sql, function(err, rows, fields) {
+		var GA = require("googalytics");
+		GA.initialize('UA-42619372-2', 'trackathon.com', function () {
+		  GA.trackPage('Money Transfer Rate', '/bestrate/'+amount_in_usd, function (err, resp) {
+		    if (!err, resp.statusCode === 200) {
+		      console.log('it worked!');
+		    }
+		  });
+		});
 		res.send(JSON.stringify(rows));  
 	});
 	console.log(query.sql);

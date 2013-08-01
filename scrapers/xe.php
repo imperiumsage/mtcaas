47a85cc25981a9331391287d7d731cfae78ecfa4
Sai Shenoy <sshenoy@kabam.com>
2013-08-01 00:23:07 -0700

    fixed xe.php

--- scrapers/xe.php
+++ scrapers/xe.php
@@ -8,7 +8,7 @@ $hour = date('H');
 $html = file_get_html("http://www.xe.com/staticcharts/get/?INR,USD,1,1&cs=1");
 $lastRate = $html->find('div[class="HLC"]',0)->lastChild()->plaintext;
 
-$sql = "insert into hourly_bank_rate(date,hour,rate) values('$date',$hour,$lastRate) on duplicate key update rate = $lastRate";
+$sql = "insert into hourly_bank_rate(date,hour,rate) values('$today',$hour,$lastRate) on duplicate key update rate = $lastRate";
 print $sql."\n";
 if(!$mysqli->query($sql)) {
 	echo "Query failed: (" . $mysqli->errno . ") " . $mysqli->error;
